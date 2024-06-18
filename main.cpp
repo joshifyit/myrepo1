d741931fa56b958e4243bc04847aaf2b40c0e716
joshifyit <jodsouza_b20@et.vjti.ac.in>
2024-06-18 08:36:35 +0530

    Sorting function added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include<vector>
+
+using namespace std;
+
 void printingArray(vector<int> arr, int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+    return;
+}
+
+void sorting(int n){
+    //sorting code
     return;
 }
 int main(){
